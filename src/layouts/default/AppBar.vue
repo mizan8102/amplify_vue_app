073d9670986e9f1b457a4d28c38d897880f04748
miza8102 <mizan.prog@gmail.com>
2023-06-24 12:42:29 +0600

    uploading-asw-amplify

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,45 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
+  <v-app-bar color="primary" prominent>
+    <v-app-bar-nav-icon variant="text" @click.stop="rail = !rail"></v-app-bar-nav-icon>
 
-      Essentials Preset
-    </v-app-bar-title>
+    <v-toolbar-title>AWS</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn variant="text" icon="mdi-magnify"></v-btn>
+
+    <v-btn variant="text" icon="mdi-filter"></v-btn>
+
+    <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
   </v-app-bar>
+  <v-navigation-drawer v-model="drawer" :rail="rail" permanent @click="rail = false">
+    <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg" title="John Leider" nav>
+      <template v-slot:append>
+        <v-btn variant="text" icon="mdi-chevron-left" @click.stop="rail = !rail"></v-btn>
+      </template>
+    </v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-list density="compact" nav>
+      <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
+      <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
+      <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
-<script lang="ts" setup>
-  //
+<script>
+export default {
+  data: () => ({
+    drawer: true,
+    rail: true
+  }),
+
+  watch: {
+    group() {
+      this.drawer = false
+    },
+  },
+}
 </script>
